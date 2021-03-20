0bcbbdb5ab615a48178780aad6fa6cfa72bc3894
Aditya Chavan <adityachavan198@gmail.com>
2021-03-20 23:35:36 +0530

    Admin

--- selfrefresh/refresh/admin.py
+++ selfrefresh/refresh/admin.py
@@ -9,7 +9,7 @@ class UserAdmin(admin.ModelAdmin):
 class TriviaStoreAdmin(admin.ModelAdmin):
     list_display=['tid', 'cid','question','option1','option2', 'option3', 'option4', 'answer', 'likes', 'dislikes', 'displayed']
     search_fields=['tid', 'cid','question','option1','option2', 'option3', 'option4', 'answer', 'likes', 'dislikes', 'displayed']
-    list_filter=['tid', 'cid','question','option1','option2', 'option3', 'option4', 'answer', 'likes', 'dislikes', 'displayed']
+    list_filter=['cid', 'likes', 'dislikes', 'displayed']
 
 class ClusterAdmin(admin.ModelAdmin):
     list_display=['cid', 'cname']
@@ -26,3 +26,6 @@ admin.site.register(UserOfApp,UserAdmin)
 admin.site.register(TriviaStore,TriviaStoreAdmin)
 admin.site.register(Cluster,ClusterAdmin)
 admin.site.register(Score,ScoreAdmin)
+admin.site.site_header = "SelfRefresh Admin"
+admin.site.site_title = "SelfRefresh Admin Page"
+admin.site.index_title = "SelfRefresh Admin Page"
