a886aed212d9c9553bc4247027d524d9922068ed
sagarambilpure <37987124+sagarambilpure@users.noreply.github.com>
2021-03-20 21:28:28 +0530

    fetch from api

--- Self Refresh extension/content_script.js
+++ Self Refresh extension/content_script.js
@@ -1,9 +1,105 @@
+var addOverlay = async function () {
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'          
+      };
+
+      try{
+        const data = await fetch("http://127.0.0.1:8000/refresh/fetchtrivia", requestOptions);
+        console.log(data);
+        
+        // fetch("http://127.0.0.1:8000/refresh/fetchtrivia", requestOptions)
+        // .then(response => response.text())
+        // .then(result => console.log(result))
+        // .catch(error => console.log('error', error));
+
+        } catch (error) {
+            // do nothing
+        }
+
+        addOverlayHtml("Ada")
+
+ }
+ 
+
+
+function addOverlayHtml(overlayContent){
+
+    // console.log(question)
+    question="Who is the current CEO of Accenture Company?"
+    option1 = "David P Rowland"
+    option2 =  "option2"
+    option3 = "John Rowland"
+    option4 = "Rowland Mackenzie"
+
+    var htmlStart = `<div class="modal fade" id="overlay-extension" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Quick Refresh</h4>
+        </div>
+        <div class="modal-body">
+          <p>`;
+    
+
+    var html1 = `</p>
+          <div class="radio">
+            <label><input type="radio" name="optradio">`
+
+    var html2 = `</label>
+            </div>
+            <div class="radio">
+            <label><input type="radio" name="optradio">`
+    
+    var html3 = `</label>
+            </div>
+            <div class="radio">
+            <label><input type="radio" name="optradio">`
+    var html4 = 
+            `</label>
+            </div>
+            <div class="radio ">
+            <label><input type="radio" name="optradio">`
+
+    var htmlEnd = 
+                        `</label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Check</button>
+                    </div>
+                </div>
+                
+                </div>
+            </div>`;
+   
+    overlay = htmlStart + question + html1 + option1 + html2 + option2 + html3 + option3 + html4 + option4 + htmlEnd;
+    
+  $("body").append(overlay);
+}
 
 
 function clickPlanet() {
+
+   addOverlay().then(()=> {
+    console.log("here!");
+
+    $("#overlay-extension").on('hide.bs.modal', function(){
+       $("#overlay-extension").remove();
+     });
     
-    $("#overlay-extension").modal();
+     $("#overlay-extension").modal();
+   });
+   
+   
       
 }
+
+
 
 clickPlanet();
