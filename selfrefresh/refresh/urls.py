cd5c0783fd068b0634954dc7c649ae7fb796bf0b
Aditya Chavan <adityachavan198@gmail.com>
2021-03-20 18:40:40 +0530

    API

---
+++ selfrefresh/refresh/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from django.conf.urls import url
+
+urlpatterns = [
+    # path("index", views.index,name="index"),
+    path("fetchtrivia", views.fetchtrivia, name="fetchtrivia"),
+]
--- selfrefresh/refresh/views.py
+++ selfrefresh/refresh/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 # Create your views here.
+
+def fetchtrivia(request):
+    data = {
+        'tid': 1,
+        'question': 'Who is the current CEO of Accenture Company?',
+        'option1': 'David P Rowland',
+        'option2': 'Patrick Roland',
+        'option3': 'John Rowland',
+        'option4': 'Rowland Mackenzie',
+        'answer': 'David P Rowland',
+    }
+    return JsonResponse(data)
